69c7a4c14098c2318c23232d62e3e87bb1636353
James Curran <j.curran@auckland.ac.nz>
2017-08-22 14:38:50 +1200

    Changed renumChunks to handle markdown

--- R/renumChunks.R
+++ R/renumChunks.R
@@ -1,11 +1,15 @@
-renumChunks = function(fi, dummyRun = TRUE, backup = TRUE){
+renumChunks = function(fi, dummyRun = TRUE, backup = TRUE, knitr = FALSE, labelStub = NULL){
   Lines = readLines(fi$fullName)
 
   if(backup){
     writeBackup(src = fi$fName, path = fi$path, dummyRun = dummyRun)
   }
 
-  chunkLines = grep("^<<([^>]*)>>=$", Lines)
+  chunkLines = if(knitr){
+    grep("^<<([^>]*)>>=$", Lines)
+  }else{
+    grep("^[`]{3}\\{r[^}]*\\}$", Lines)
+  }
 
   chunkCounter = 0
 
@@ -13,18 +17,29 @@ renumChunks = function(fi, dummyRun = TRUE, backup = TRUE){
 
   for(lineNum in chunkLines){
     line = Lines[lineNum]
-    chunkOptions = gsub("^<<([^>]*)>>=$", "\\1", line)
+    chunkOptions = if(knitr){
+      gsub("^<<([^>]*)>>=$", "\\1", line)
+    }else{
+      gsub("^[`]{3}\\{r([^}]*)\\}$", line)
+    }
 
-    chunkLabel = paste0("RC-", handoutNumber, "-", if(chunkCounter < 10){
-                                                      "00"
-                                                    }else if (chunkCounter >=10 && chunkCounter < 100){
-                                                      "0"
-                                                    }else{
-                                                      ""
-                                                    }, chunkCounter)
+    if(is.null(labelStub)){
+      labelStub = paste0("RC-", handoutNumber, "-")
+    }
+    chunkLabel = paste0(labelStub, if(chunkCounter < 10){
+                                     "00"
+                                   }else if (chunkCounter >=10 && chunkCounter < 100){
+                                     "0"
+                                   }else{
+                                     ""
+                                   }, chunkCounter)
     chunkCounter = chunkCounter + 1
     ## remove any existing chunk label
-    pattern ="(RC-(0[1-9]|1[0-9]|20)-[0-9]{3}|R[^,>]+)(, )*"
+    pattern = if(knitr){
+      "(RC-(0[1-9]|1[0-9]|20)-[0-9]{3}|R[^,>]+)(, )*"
+    }else{
+      "(rc-cs[0-9]+[0-9]{3})(, )*"
+    }
     if(grepl(pattern, chunkOptions)){
       r = regexpr(pattern, chunkOptions)
       tmp = regmatches(chunkOptions, r)
@@ -42,7 +57,11 @@ renumChunks = function(fi, dummyRun = TRUE, backup = TRUE){
                      chunkLabel
                    }
 
-    replacementLine = paste0("<<", chunkOptions, ">>=")
+    replacementLine = if(knitr){
+      paste0("<<", chunkOptions, ">>=")
+    }else{
+      paste0("```{r ", chunkOptions, "}")
+    }
     cat(paste0(lineNum,": ", replacementLine, "\n"))
 
     if(!dummyRun){
